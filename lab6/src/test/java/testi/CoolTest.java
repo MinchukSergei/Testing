f011c0b93db38ae2efc65f4e2a861cc8d5d55a9b
Minchuk Sergei <minchuk94@gmail.com>
2016-11-19 15:14:01 +0300

    added lab6

---
+++ lab6/src/test/java/testi/CoolTest.java
@@ -0,0 +1,57 @@
+package testi;
+
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.ProxyServer;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by USER on 19.11.2016.
+ */
+public class CoolTest {
+
+    @Test
+    public void testMobProxyServer() throws Exception {
+        // запуск прокси сервера
+        ProxyServer server = new ProxyServer(4444);
+        server.start();
+
+        // получение Selenium proxy
+        Proxy proxy = server.seleniumProxy();
+
+        // конфигурация FirefoxDriver для использования прокси
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
+
+        WebDriver driver = new ChromeDriver(capabilities);
+
+        // создание HAR с меткой "yandex.ru"
+        server.newHar("adukacyja.by");
+
+        // открытие страницы
+        driver.get("http://www.adukacyja.by/");
+        driver.findElement(By.xpath(".//*[@id='essentialnavbar']/div/div/div[2]/div[1]/ul/li/a/em")).click();
+        driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("login");
+        driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("password");
+        driver.findElement(By.xpath(".//*[@id='loginbtn']")).click();
+
+        // получение данных HAR
+        Har har = server.getHar();
+        File file = new File(ClassLoader.getSystemResource("Test.har").getFile());
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            har.writeTo(fos);
+        } finally {
+            driver.quit();
+            server.stop();
+        }
+    }
+}
